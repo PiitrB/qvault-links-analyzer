1c06b0fd7c55a16946967b56e31bfc131b8b09e9
Peter <peter.bukovy@gmail.com>
2022-03-22 17:42:19 +0100

    URL normalization tested and working

--- crawl.py
+++ crawl.py
@@ -1,3 +1,4 @@
+from urllib.parse import urlparse
 from lxml import html
 
 def get_urls_from_string(string, base_url):
@@ -8,3 +9,11 @@ def get_urls_from_string(string, base_url):
         if elem.tag == "a":
             urls.append(elem.get("href"))
     return urls
+
+def normalize_url(url):
+    parsed_url = urlparse(url) #parsing URL to blocks defined in urlparse function
+    normalized_url = f"{parsed_url.netloc}{parsed_url.path}" #concacenating two blocks that define unique url
+    normalized_url = normalized_url.lower() #making all lowercase
+    while len(normalized_url) > 0 and normalized_url[-1] == "/": #while last character of URL is /, remove it.
+            normalized_url = normalized_url[:-1]
+    return normalized_url
--- tests.py
+++ tests.py
@@ -1,7 +1,8 @@
+from re import A
 import unittest
 from report import remove_none_values
 from report import sort_pages
-from crawl import get_urls_from_string
+from crawl import get_urls_from_string, normalize_url
 
 
 
@@ -20,6 +21,13 @@ class Tests(unittest.TestCase):
     def test_get_urls(self):
         self.assertEqual(["https://qvault.io"],get_urls_from_string('<html><body><a href="https://qvault.io"><span>Qvault></span></a></body></html>',"https://qvault.io"))
         self.assertEqual(["https://qvault.io", "https://wagslane.dev"],get_urls_from_string('<html><body><a href="https://qvault.io"><span>Qvault></span></a><a href="https://wagslane.dev"><span>Qvault></span></a></body></html>',"https://qvault.io"))
+
+    def test_normalize_url(self):
+        self.assertEqual("", normalize_url(""))
+        self.assertEqual("", normalize_url("/"))
+        self.assertEqual("docs.python.org/3/library/urllib.parse.html", normalize_url("https://docs.PYTHON.org/3/LIBRARY/urllib.parse.html#url-parsing"))
+        self.assertEqual("www.docs.python.org/3/library/urllib.parse.html", normalize_url("https://www.docs.PYTHON.org/3/LIBRARY/urllib.parse.html#url-parsing"))
+        self.assertEqual("qvault.io", normalize_url("http://qvault.io/"))
 
 
 if __name__ == "__main__":
